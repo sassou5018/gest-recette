b0424cc4292d26fe0f799253bf3f6f1050e7004c
Yassine Ben Ahmed <yassinebenahmede@gmail.com>
2023-07-13 08:45:14 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import router from './router'
+
+function App() {
+  return (
+    <>
+    <ChakraProvider>
+      <RouterProvider router={router}/>
+    </ChakraProvider>
+    </>
+  )
+}
+
+export default App
